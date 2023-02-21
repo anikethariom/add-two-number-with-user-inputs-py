96bb024aff3b53b0061dc4340535ece9918ce51d
anikethariom <124948208+anikethariom@users.noreply.github.com>
2023-02-21 20:30:17 +0530

    Add files via upload

---
+++ whit user-inputs.py
@@ -0,0 +1,14 @@
+ # solution-1 (with pre-defined variables )
+
+#num1 = 30
+# num2 = 22
+#sum = num1+num2
+#print (" this sum of given two numbers is ", sum)
+
+#solution-2(with user-inputs)
+num1 =float (input("enter a number here :"))
+num2 =float (input("enter another number here :"))
+
+sum=num1+num2
+
+print ("the sum of the provided two numbers is",sum)
